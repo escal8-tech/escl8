4f12f12d52d69233593fda3486ec9f04d1f6e14d
Namith <namithspider@gmail.com>
2026-03-21 11:56:54 +0530

    fix the refrehs system smoothness loading whole page is removed

--- src/app/portal/upload/page.tsx
+++ src/app/portal/upload/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import PortalAuthProvider from "@/components/PortalAuthProvider";
 import { UploadContent } from "./components/UploadContent";
 
 export default function PortalUploadPage() {
-  return (
-    <PortalAuthProvider>
-      <UploadContent />
-    </PortalAuthProvider>
-  );
+  return <UploadContent />;
 }
