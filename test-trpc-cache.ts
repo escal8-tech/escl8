2ec96bba276eb5f92df7f99c57400afae86a5789
Namith <namithspider@gmail.com>
2026-06-21 02:11:07 +0530

    chore: remove scratch files to fix lint

--- test-trpc-cache.ts
+++
@@ -1,5 +0,0 @@
-import { initTRPC } from '@trpc/server';
-const t = initTRPC.create();
-const cacheMiddleware = t.middleware(async ({ next }) => {
-  return { ok: true, data: "cached", marker: "middlewareMarker" as any };
-});
